ec1ddb5b8aefb95fda14134cbc2de0df79430efd
william <william.guan12@gmail.com>
2021-10-08 15:59:11 -0400

    feat: added in seed colors to the Palette component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Color Picker App</h1>
+        <Palette {...seedColors[4]} />
       </div>
     );
   }
