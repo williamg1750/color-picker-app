b8521c94110a22491f964d429d8d1a0875c708ba
william <william.guan12@gmail.com>
2021-10-08 15:43:20 -0400

    feat: minor changes to class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Color Picker App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Color Picker App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
