9309d767545596d3890e28f3d5800a018bd1ec1e
william <william.guan12@gmail.com>
2021-10-08 15:58:17 -0400

    feat: created palette where the colors would be

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/*navbar goes here*/}
+        <div className='Palette-colors'>
+          {/*bunch of color boxes goes here*/}
+        </div>
+        {/*Footer goes here*/}
+      </div>
+    );
+  }
+}
+
+export default Palette;
