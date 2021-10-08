9f399baeca9402b6ab905c680cdf32ab9d422293
william <william.guan12@gmail.com>
2021-10-08 17:23:19 -0400

    feat: create ColorBox to show a one color from the seed data and also a map to view each color in the array data

--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 
 class Palette extends Component {
   render() {
+    let colorBoxes = this.props.colors.map((currColor) => (
+      <ColorBox
+        background={currColor.color}
+        name={currColor.name}
+        key={currColor.color}
+      />
+    ));
     return (
       <div className='Palette'>
         {/*navbar goes here*/}
         <div className='Palette-colors'>
           {/*bunch of color boxes goes here*/}
+          {colorBoxes}
         </div>
         {/*Footer goes here*/}
       </div>
