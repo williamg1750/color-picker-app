416a76fde062361f24d73ec9d5b8bce764e656a8
william <william.guan12@gmail.com>
2021-10-09 08:03:12 -0400

    Style: added transition effect when clicking on the color box to copy color code

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,33 @@ import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  //changeCopyState handles the click and sets copied to true which triggers css to do a effect and changeCopyState also takes a callback settime out which sets copied to false after 1500ms which ends the css efect
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
+
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className='ColorBox'>
+          {/* {copied && 'show'} basically means that if copied is true it adds in show to the class name*/}
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && 'show'}`}
+          />
+          <div className={`copy-msg ${copied && 'show'}`}>
+            <h1>Copied</h1>
+            <p>{background}</p>
+          </div>
           <div className='copy-container'>
             <div className='box-content'>
               <span>{name}</span>
