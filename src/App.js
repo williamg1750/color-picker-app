a2f768ea63e7bf9d1ed49c142f3bff2be57d6388
william <william.guan12@gmail.com>
2021-10-22 14:22:16 -0400

    feat: react router dom added and create two basic paths

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelper';
+import { Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
-    console.log(generatePalette(seedColors[4]));
+    console.log(seedColors);
     return (
       <div className='App'>
-        <Palette palette={generatePalette(seedColors[4])} />
+        <Switch>
+          <Route exact path='/' render={() => <h1>Palette goes here</h1>} />
+          <Route
+            exact
+            path='/palette/:id'
+            render={(routeProps) => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
       </div>
     );
   }
